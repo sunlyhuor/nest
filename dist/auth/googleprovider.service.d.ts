ecbdb34bd20fc9005c0b2d5b6aceb3eab05be377
sunlyhuor <96644663+sunlyhuor@users.noreply.github.com>
2023-03-17 10:49:36 +0700

    Delete dist directory

--- dist/auth/googleprovider.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Strategy, VerifyCallback } from "passport-google-oauth2";
-declare const GoogleProviderService_base: new (...args: any[]) => Strategy;
-export declare class GoogleProviderService extends GoogleProviderService_base {
-    constructor();
-    validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any>;
-}
-export {};
